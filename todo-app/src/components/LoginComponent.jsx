36e158a1cd788163400e74282b5c3697210c1225
Roberto-Livi <robertomlivi@outlook.com>
2021-03-02 16:14:44 -0600

    Adding hardcoded authentication to LoginComponent

--- todo-app/src/components/LoginComponent.jsx
+++ todo-app/src/components/LoginComponent.jsx
@@ -4,22 +4,55 @@ class LoginComponent extends Component {
 
     state = {
         username: "",
-        password: ""
+        password: "",
+        hasLoginFailed: false,
+        showSuccessMessage: false
     }
 
-    handleUsernameChange = (event) => {
+    handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         })
     }
+
+    loginClicked = () => {
+        if(this.state.username === 'robbie' && this.state.password === 'loslobos'){
+            console.log('Successful')
+            this.setState({ showSuccessMessage: true })
+            this.setState({ hasLoginFailed: false})
+        } else {
+            console.log('failed')
+            this.setState({ showSuccessMessage: false})
+            this.setState({ hasLoginFailed: true })
+        }
+    }
+
     render() {
         return(
             <div>
-                Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                <button>Login</button>
+            <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
+            <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} />
+                Username: <input type="text" name="username" value={this.state.username} onChange={(e) => this.handleChange(e)} />
+                Password: <input type="password" name="password" value={this.state.password} onChange={(e) => this.handleChange(e)} />
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         )
+    }
+}
+
+function ShowInvalidCredentials(props) {
+    if(props.hasLoginFailed) {
+        return <div>Invalid Credentials</div>
+    } else {
+        return null
+    }
+}
+
+function ShowLoginSuccessMessage(props) {
+    if(props.showSuccessMessage) {
+        return <div>Login Successful</div>
+    } else {
+        return null
     }
 }
 
