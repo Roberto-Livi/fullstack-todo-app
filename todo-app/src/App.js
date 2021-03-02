34995208b535092a53dfa681c513e63eaabc52ad
Roberto-Livi <robertomlivi@outlook.com>
2021-03-02 15:13:56 -0600

    added TodoApp and LoginComponent components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TodoApp from './components/TodoApp'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        My Hello World
+        <TodoApp />
       </div>
     );
   }
