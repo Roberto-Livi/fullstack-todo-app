532ecb4fb41f21b55ef72ef7434a04f2dd467c7e
Roberto-Livi <robertomlivi@outlook.com>
2021-03-02 15:32:08 -0600

    adding state (username, password) to the LoginComponent as well as an onChange handler

--- todo-app/src/components/LoginComponent.jsx
+++ todo-app/src/components/LoginComponent.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react'
 
 class LoginComponent extends Component {
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render() {
         return(
             <div>
-                Username: <input type="text" name="username"/>
-                Password: <input type="password" name="password"/>
+                Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
                 <button>Login</button>
             </div>
         )
