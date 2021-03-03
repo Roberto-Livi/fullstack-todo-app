91bcd84ea53451ad449733b4b1d9b809eaf4174c
Roberto-Livi <robertomlivi@outlook.com>
2021-03-03 15:58:46 -0600

    applied conditional rendering with && in React JSX

--- todo-app/src/components/LoginComponent.jsx
+++ todo-app/src/components/LoginComponent.jsx
@@ -30,29 +30,13 @@ class LoginComponent extends Component {
     render() {
         return(
             <div>
-            <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
-            <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} />
+            {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+            {this.state.showSuccessMessage && <div>Login Successful</div>}
                 Username: <input type="text" name="username" value={this.state.username} onChange={(e) => this.handleChange(e)} />
                 Password: <input type="password" name="password" value={this.state.password} onChange={(e) => this.handleChange(e)} />
                 <button onClick={this.loginClicked}>Login</button>
             </div>
         )
-    }
-}
-
-function ShowInvalidCredentials(props) {
-    if(props.hasLoginFailed) {
-        return <div>Invalid Credentials</div>
-    } else {
-        return null
-    }
-}
-
-function ShowLoginSuccessMessage(props) {
-    if(props.showSuccessMessage) {
-        return <div>Login Successful</div>
-    } else {
-        return null
     }
 }
 
