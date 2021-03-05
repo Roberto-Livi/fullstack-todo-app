c4290ccf7c086b141f08de935cb5b893954c173f
Roberto-Livi <robertomlivi@outlook.com>
2021-03-04 20:58:51 -0600

    Implementing Routing for the Login and Welcome component with React Routes

--- todo-app/src/components/TodoApp.jsx
+++ todo-app/src/components/TodoApp.jsx
@@ -1,13 +1,18 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Login from './Login'
 import Welcome from './Welcome'
+
 
 class TodoApp extends Component {
     render() {
         return(
             <div className="TodoApp">
-                <Login />
-                <Welcome />
+                <Router>
+                    <Route path="/" exact component={Login} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/welcome" component={Welcome} />
+                </Router>
             </div>
         )
     }
