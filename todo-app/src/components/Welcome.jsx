5d4b0e4ceddb7eda732e2cfa492731bdf17d698f
Roberto-Livi <robertomlivi@outlook.com>
2021-03-08 19:24:16 -0600

    Preparing Welcome component to connect to java backend with a click of a button

--- todo-app/src/components/Welcome.jsx
+++ todo-app/src/components/Welcome.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Welcome extends Component {
+
+    retrieveWelcomeMessage = () => {
+            console.log("retrieve clicked");
+        }
     render() {
         return(
             <>
                 <h1>Welcome!</h1>
-                <div className="container">Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link></div>
+                <div className="container">
+                    Welcome {this.props.match.params.name}.
+                    You can manage your todos <Link to="/todos">here</Link>
+                </div>
+                <div className="container">
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div>
             </>
         )
     }
