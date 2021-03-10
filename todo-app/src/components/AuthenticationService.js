f51255cd665e87a410c3db06f11569b9c77193e1
Roberto-Livi <robertomlivi@outlook.com>
2021-03-10 12:41:05 -0600

    executing delete requests with chrome extension

--- todo-app/src/components/AuthenticationService.js
+++ todo-app/src/components/AuthenticationService.js
@@ -1,20 +1,25 @@
 class AuthenticationService {
-    registerSuccessfulLogin(username, password){
+    registerSuccessfulLogin = (username, password) => {
         console.log("Register Successful Login");
         sessionStorage.setItem('authenticatedUser', username);
     }
 
-    logout() {
+    logout = () => {
         sessionStorage.removeItem('authenticatedUser');
     }
 
-    isUserLoggedIn() {
+    isUserLoggedIn = () => {
         let user = sessionStorage.getItem('authenticatedUser');
         if(user === null) {
             return false;
         } else {
             return true;
         }
+    }
+
+    getLoggedInUsername = () => {
+        let username = sessionStorage.getItem('authenticatedUser');
+        return true && username
     }
 }
 
--- todo-app/src/components/ListTodos.jsx
+++ todo-app/src/components/ListTodos.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import TodoDataService from '../api/TodoDataService.js'
+import AuthenticationService from './AuthenticationService.js'
 
 class ListTodos extends Component {
 
     state = {
         todos: [
-            {id: 1, description: "Obtain AWS Developer Cert", done: false, targetDate: new Date()},
-            {id: 2, description: "Go to Cancun", done: false, targetDate: new Date()},
-            {id: 3, description: "Visit Japan", done: false, targetDate: new Date()}
+            // {id: 1, description: "Obtain AWS Developer Cert", done: false, targetDate: new Date()},
+            // {id: 2, description: "Go to Cancun", done: false, targetDate: new Date()},
+            // {id: 3, description: "Visit Japan", done: false, targetDate: new Date()}
         ]
+    }
+
+    componentDidMount() {
+        let username = AuthenticationService.getLoggedInUsername
+        TodoDataService.retrieveAllTodos(username)
+        .then(
+            response => {
+                this.setState({todos: response.data})
+            }
+        )
     }
     render() {
         return(
