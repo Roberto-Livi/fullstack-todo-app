622af02e5b7a45d9f34dda0e66435dfbc6399980
Roberto-Livi <robertomlivi@outlook.com>
2021-03-11 11:28:20 -0600

    created the POST request method

--- restful-web-services/src/main/java/com/livi/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/livi/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -34,10 +36,19 @@ public class TodoResource {
         return ResponseEntity.notFound().build();
     }
 
-    @PutMapping
+    @PutMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
         Todo todoUpdated = todoService.save(todo);
         return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.save(todo);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+
+        // returns a status of created and returns the ui of the created resource
+        return ResponseEntity.created(uri).build();
     }
 
 
